fc0d07f8409ba0087189969f06ac48e5aa13aa95
kanshemeza <35347564+kanshemeza@users.noreply.github.com>
2018-05-02 19:48:24 -0500

    Added a model of my products and sample products

--- OrganicFarmStore/Controllers/HomeController.cs
+++ OrganicFarmStore/Controllers/HomeController.cs
@@ -10,9 +10,55 @@ namespace OrganicFarmStore.Controllers
 {
     public class HomeController : Controller
     {
+        private List<Product> _products;
+        public HomeController()
+        {
+            _products = new List<Product>();
+            {
+                _products.Add(new Product
+                {
+                    ID = 4,
+                    Name = "Green Bell Pepper",
+                    Description = "Et nisl adhuc interpretaris cum, " +
+                    "at mea equidem indoctum neglegentur. Odio eripuit suscipit sed no, etiam ",
+                    Image = "/images/green-bell-pepper.jpg",
+                    Price = 3.95m
+                });
+
+                _products.Add(new Product {
+                    ID = 5,
+                    Name = "Green Onions",
+                    Description = "Sed ut perspiciatis, unde omnis iste natus error sit " +
+                    "voluptatem accusantium doloremque laudantium",
+                    Image = "/images/green-onions.jpg",
+                    Price = 1.99m
+                });
+
+                _products.Add(new Product
+                {
+                    ID = 6,
+                    Name = "Strawberries",
+                    Description = "Et nisl adhuc interpretaris cum, " +
+                    "at mea equidem indoctum neglegentur. Odio eripuit suscipit sed no, etiam ",
+                    Image = "/images/strawberries.jpg",
+                    Price = 3.95m
+                });
+            }
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_products);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id.HasValue)
+            {
+                Product p = _products.Single(x => x.ID == id.Value);
+                return View(p);
+            }
+            return NotFound();
+
         }
 
         public IActionResult About()
---
+++ OrganicFarmStore/Controllers/ProductController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OrganicFarmStore.Models;
+
+namespace OrganicFarmStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private List<Product> _products;
+
+        public ProductController()
+        {
+            _products = new List<Product>();
+
+            _products.Add(new Product
+            {
+                ID = 1,
+                Name = "Tomatoes",
+                Description = "Lorem ipsum dolor sit amet, " +
+                "nec reque platonem philosophia ei. Dolores " +
+                "salutandi voluptatibus sit no",
+                Image = "/images/tomatoes.jpg",
+                Price = 6.97m,
+                Category = "Fresh"
+            });
+            _products.Add(new Product
+            {
+                ID = 2,
+                Name = "Raspberry",
+                Description = "Virtute propriae " +
+                "honestatis ad vim, habeo inciderint adversarium vix ea, " +
+                "luptatum reprehendunt an mea",
+                Image = "/images/raspberries.jpg",
+                Price = 2.97m,
+                Category = "Fresh"
+            });
+        }
+        public IActionResult Index()
+        {
+            return View(_products);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id.HasValue)
+            {
+                Product p = _products.Single(x => x.ID == id.Value);
+                return View(p);
+            }
+            return NotFound();
+           
+        }
+    }
+}
